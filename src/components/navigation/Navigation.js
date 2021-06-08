0cbbe5ba2c36cf48ee3790e1cdfaad3286d67b9a
Dan <dan.tran301@gmail.com>
2021-06-08 16:50:46 -0700

    Navbar
    Implement logout feature and have navbar links change based on user
    being logged in or out.

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+// redux
+import { useDispatch } from "react-redux";
+import { LOGOUT } from "../../redux/users/usersTypes";
+// dependencies
+import decode from "jwt-decode";
+// styles
 import { Navbar, Nav } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
+// react-router
+import { useHistory, useLocation, NavLink } from "react-router-dom";
 
 function Navigation() {
+  const location = useLocation();
+  const history = useHistory();
+  const dispatch = useDispatch();
+  // retrieving our user from local storage
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
+
+  const handleLogout = () => {
+    dispatch({ type: LOGOUT });
+    history.push("/signup");
+    setUser(null);
+  };
+  console.log(user);
+  useEffect(() => {
+    const token = user?.token;
+
+    if (token) {
+      const decodedToken = decode(token);
+    }
+
+    setUser(JSON.parse(localStorage.getItem("profile")));
+  }, [location]);
   return (
     <Navbar bg="light" expand="lg">
       <Navbar.Brand>
@@ -11,8 +39,21 @@ function Navigation() {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
-          <NavLink to="/createWorkout">Create Workout</NavLink>
-          <NavLink to="/signup">Register</NavLink>
+          <NavLink className="mx-3" to="/createWorkout">
+            Create Workout
+          </NavLink>
+          <NavLink className="mx-3" to="/signup">
+            Register
+          </NavLink>
+          {user?.result ? (
+            <NavLink onClick={handleLogout} className="mx-3" to="/signup">
+              Logout
+            </NavLink>
+          ) : (
+            <NavLink className="mx-3" to="/signup">
+              Sign in
+            </NavLink>
+          )}
         </Nav>
       </Navbar.Collapse>
     </Navbar>
